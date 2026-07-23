46c5e82254d8d0eb22eb8ccd48e8e215281a057a
Chhavi Jain <jchhavi2317@gmail.com>
2026-07-23 14:28:09 +0530

    Fix SQLite instance folder

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-
 with app.app_context():
     db.create_all()
 
--- config.py
+++ config.py
@@ -2,14 +2,13 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+INSTANCE_FOLDER = os.path.join(BASE_DIR, "instance")
+os.makedirs(INSTANCE_FOLDER, exist_ok=True)
+
 
 class Config:
-
-    SQLALCHEMY_DATABASE_URI = \
-        "sqlite:///" + os.path.join(
-            BASE_DIR,
-            "instance",
-            "portfolio.db"
-        )
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" + os.path.join(INSTANCE_FOLDER, "portfolio.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
